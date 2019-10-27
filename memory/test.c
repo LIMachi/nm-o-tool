1914d43449bb6495c1bb8438208bf65dc17cd526
LIMachi <lee.mach.hm@gmail.com>
2019-10-27 11:34:32 +0100

    the 4 resize of memory endian based should be good now, the ternaris use the property of the unsigned to underflow on negatives to only test half the range [0-size[ : index < size

--- memory/test.c
+++ memory/test.c
@@ -6,21 +6,23 @@ int	main(void)
 	size_t			size1 = 8;
 	size_t			size2 = 4;
 	int				endian1 = 4321;
-	int				endian2 = 1234;
-	unsigned char mem1[8] = {7, 6, 5, 4, 3, 2, 1, 0};
-	unsigned char mem2[8];
+	int				endian2 = 4321;
+	unsigned char	mem1[8] = {7, 6, 5, 4, 3, 2, 1, 0};
+	unsigned char	mem2[8];
 
 	for (sw = 0; sw < size1; ++sw)
 		printf("%hhu%c", mem1[sw], sw + 1 < size1 ? ' ' : '\n');
-	// for (; sw < 8; ++sw)
-	// 	mem1[sw] = 42;
-	printf("e: %0.4d - s: %zu -> e: %0.4d - s: %zu\n", endian1, size1, endian2, size2);
+	printf("e: %.4d - s: %zu -> e: %.4d - s: %zu\n", endian1, size1, endian2, size2);
 	for (sw = 0; sw < size2; ++sw)
 	{
 		if (endian1 == 1234 && endian2 == 4321)
-			mem2[sw] = size2 - sw - 1 >= 0 && size2 - sw - 1 < size1 ? mem1[size2 - sw - 1] : 0;
+			mem2[sw] = size2 - sw - 1 < size1 ? mem1[size2 - sw - 1] : 0;
 		if (endian1 == 4321 && endian2 == 1234)
-			mem2[sw] = size1 - sw - 1 >= 0 && sw + 1 > 0 ? mem1[size1 - sw - 1] : 0;
+			mem2[sw] = size1 >= sw + 1 ? mem1[size1 - sw - 1] : 0;
+		if (endian1 == 1234 && endian2 == 1234)
+			mem2[sw] = sw < size1 ? mem1[sw] : 0;
+		if (endian1 == 4321 && endian2 == 4321)
+			mem2[sw] = sw + size1 - size2 < size1 ? mem1[sw + size1 - size2] : 0;
 		printf("%hhu%c", mem2[sw], sw + 1 < size2 ? ' ' : '\n');
 	}
 }
